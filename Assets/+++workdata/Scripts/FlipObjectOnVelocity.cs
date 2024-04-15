b93ae70632b40fb13a0f02346b796601586b8d5f
TomGrzembke <tomgrzembke04@gmail.com>
2024-04-15 16:39:32 +0100

    Fixed flip on velocity bug

--- Assets/+++workdata/Scripts/FlipObjectOnVelocity.cs
+++ Assets/+++workdata/Scripts/FlipObjectOnVelocity.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-/// <summary> Flips the object with context</summary>
+/// <summary> Flips the object with rb context</summary>
 public class FlipObjectOnVelocity : MonoBehaviour
 {
     #region serialized fields
@@ -12,7 +12,7 @@ public class FlipObjectOnVelocity : MonoBehaviour
 
     #region private fields
     Vector3 targetScale;
-    Vector3 localScale;
+    Vector3 localScale => transform.localScale;
     bool flipState;
     float maxScale;
     Rigidbody2D rb;
@@ -34,21 +34,19 @@ public class FlipObjectOnVelocity : MonoBehaviour
     {
         if (rb.velocity.magnitude <= flipSensitivity) return;
 
-        if (flipRoutine == null)
-            flipRoutine = StartCoroutine(Flip());
-
+        flipRoutine ??= StartCoroutine(Flip());
+        
     }
 
     IEnumerator Flip()
     {
         flipState = rb.velocity.x > 0;
-        localScale = transform.localScale;
         targetScale = localScale;
         targetScale.x = flipState ? -maxScale : maxScale;
 
         float flipTime = 0;
 
-        while (timeToFlip > flipTime && flipState == rb.velocity.x > 0)
+        while (timeToFlip > flipTime)
         {
             flipTime += Time.deltaTime;
             transform.localScale = Vector3.Lerp(localScale, targetScale, flipCurve.Evaluate(flipTime / timeToFlip));

0edfc9e569099c48aa8969facb412152098e22d8
TomGrzembke <tomgrzembke04@gmail.com>
2024-04-23 10:00:56 +0100

    Arena layout

---
+++ Assets/+++workdata/Scripts/Tournament/CharManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CharManager : MonoBehaviour
+{
+    #region serialized fields
+    [field: SerializeField] public GameObject[] CharPrefabs { get; private set; } 
+    [field: SerializeField] public GameObject[] Chars { get; private set; } 
+    #endregion
+
+    #region private fields
+    
+    #endregion
+
+}

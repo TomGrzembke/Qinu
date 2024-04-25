97c1e7c1218d2050579a83e5e7ededd589a6a6cd
TomGrzembke <tomgrzembke04@gmail.com>
2024-04-25 13:07:30 +0100

    Calc which chars will play

--- Assets/+++workdata/Scripts/Tournament/TournamentManager.cs
+++ Assets/+++workdata/Scripts/Tournament/TournamentManager.cs
@@ -1,6 +1,6 @@
 using MyBox;
-using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -25,11 +25,13 @@ public class TournamentManager : MonoBehaviour
     [SerializeField] float RoundsTilWin = 5;
     [field: SerializeField] public List<CharacterStats> CharStats { get; private set; }
     [field: SerializeField] public List<GameObject> AvailableChars { get; private set; }
+    [field: SerializeField] public GameMode CurrentGameMode { get; private set; }
+    [field: SerializeField] public List<GameObject> rightPlayers { get; private set; }
+    [field: SerializeField] public List<GameObject> leftPlayers { get; private set; }
     #endregion
 
     #region private fields
-    [field: SerializeField] public GameMode CurrentGameMode { get; private set; }
-
+    GameObject lastPlayed;
     #endregion
     void Start()
     {
@@ -64,46 +66,84 @@ public class TournamentManager : MonoBehaviour
         RoundAmount++;
     }
 
+    void Calc1v1()
+    {
+        ClearSideLists();
+        leftPlayers.Add(AvailableChars[0]);
+        lastPlayed = GetLowestPlayRate(lastPlayed);
+        rightPlayers.Add(GetLowestPlayRate());
+    }
 
+    void ClearSideLists()
+    {
+        leftPlayers.Clear();
+        rightPlayers.Clear();
+    }
+
+    void Calc1v1Watch()
+    {
+        ClearSideLists();
+        var first = GetLowestPlayRate();
+        leftPlayers.Add(first);
+        rightPlayers.Add(GetLowestPlayRate(first));
+    }
+    void Calc2v2()
+    {
+        ClearSideLists();
+        leftPlayers.Add(AvailableChars[0]);
+        GameObject first = GetLowestPlayRate();
+        GameObject second = GetLowestPlayRate(first);
+        GameObject third = GetLowestPlayRate(first, second);
+        leftPlayers.Add(third);
+        rightPlayers.Add(second);
+        rightPlayers.Add(first);
+    }
 
     #region Rounds
     int RoundZero()
     {
-        CharManager.Instance.PathGOTo(AvailableChars[1], MinigameManager.Instance.DefaultPosLeft
-            [Random.Range(0, MinigameManager.Instance.DefaultPosLeft.Length)].position);
+        CharManager.Instance.PathGOTo(AvailableChars[2], GetRandomDefaultPos(1));
         CurrentGameMode = GameMode.Bodi;
-
+        leftPlayers.Add(AvailableChars[0]);
+        rightPlayers.Add(AvailableChars[2]);
         return 0;
     }
     int FirstRound()
     {
         CurrentGameMode = GameMode.a1v1;
 
+        Calc1v1();
         return 0;
     }
+
+
     int SecondRound()
     {
         CurrentGameMode = GameMode.a1v1;
+        Calc1v1();
 
+        return 0;
+    }
+    int ThirdRound()
+    {
+        CurrentGameMode = GameMode.a2v2;
+        Calc2v2();
+
+        return 0;
+    }
+    int FourthRound()
+    {
+        CurrentGameMode = GameMode.watch1v1;
+        Calc1v1Watch();
         return 0;
     }
     int RandomCalcRound()
     {
         CurrentGameMode = (GameMode)Random.Range(0, 2);
-
-        return 0;
-    }
-
-    int FourthRound()
-    {
-        CurrentGameMode = GameMode.watch1v1;
-
-        return 0;
-    }
-
-    int ThirdRound()
-    {
-        CurrentGameMode = GameMode.a2v2;
+        if (CurrentGameMode == GameMode.a1v1)
+            Calc1v1();
+        else if (CurrentGameMode == GameMode.a2v2)
+            Calc2v2();
 
         return 0;
     }
@@ -116,6 +156,25 @@ public class TournamentManager : MonoBehaviour
             return MinigameManager.Instance.DefaultPosLeft[Random.Range(0, MinigameManager.Instance.DefaultPosLeft.Length)].position;
         else
             return MinigameManager.Instance.DefaultPosRight[Random.Range(0, MinigameManager.Instance.DefaultPosRight.Length)].position;
+    }
+    GameObject GetLowestPlayRate(GameObject exclude = null, GameObject exclude2 = null)
+    {
+        GameObject lowestPlayRateChar = new();
+        int lowestTimesPlayed = 100;
+        for (int i = 1; i < CharStats.Count; i++)
+        {
+            if (CharStats[i].TimesPlayed == lowestTimesPlayed)
+                if (Random.Range(0, 2) == 0) continue;
+
+            if (CharStats[i].TimesPlayed <= lowestTimesPlayed)
+            {
+                if (exclude == CharStats[i].CharGO || exclude2 == CharStats[i].CharGO) continue;
+
+                lowestTimesPlayed = CharStats[i].TimesPlayed;
+                lowestPlayRateChar = CharStats[i].CharGO;
+            }
+        }
+        return lowestPlayRateChar;
     }
 }
 

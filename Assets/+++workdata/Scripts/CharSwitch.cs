bc91e3d557c17cbed28fced3738ec39c45054545
TomGrzembke <tomgrzembke04@gmail.com>
2024-06-21 11:33:09 +0100

    Char switch start

---
+++ Assets/+++workdata/Scripts/CharSwitch.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSwitch : MonoBehaviour
+{
+    #region serialized fields
+    
+    #endregion
+
+    #region private fields
+    
+    #endregion
+
+    public void Calculate(List<GameObject> leftPlayers, List<GameObject> rightPlayers)
+    {
+
+    }
+
+}

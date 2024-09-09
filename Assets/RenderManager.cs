402893c6a55ca37f08a786f703af1d1ca88aff3b
DESKTOP-1393E8M\Fujitsu <tomgrzembke04@gmail.com>
2024-09-10 00:46:16 +0200

    Render manager start

---
+++ Assets/RenderManager.cs
@@ -0,0 +1,21 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RenderManager : MonoBehaviour
+{
+    RenderTexture renderTexture;
+    Camera mainCam;
+    void Awake()
+    {
+        mainCam = Camera.main;
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}

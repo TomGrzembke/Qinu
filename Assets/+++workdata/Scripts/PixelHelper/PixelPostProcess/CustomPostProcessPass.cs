47fc9eec0a51955140f0ca30c89cc418d69d16a8
TomGrzembke <tomgrzembke04@gmail.com>
2024-09-19 12:45:52 +0100

    Shader setup

--- Assets/+++workdata/Scripts/PixelHelper/PixelPostProcess/CustomPostProcessPass.cs
+++ Assets/+++workdata/Scripts/PixelHelper/PixelPostProcess/CustomPostProcessPass.cs
@@ -6,12 +6,14 @@ using UnityEngine.Rendering.Universal;
 [System.Serializable]
 public class CustomPostProcessPass : ScriptableRenderPass
 {
-    RenderTextureDescriptor descriptor;
     RTHandle cameraColorTarget;
     RTHandle cameraDepthTarget;
 
     Material m_render;
     Material m_composite;
+
+    PixelPostProcessComponent m_effect;
+    RenderTextureDescriptor m_Descriptor;
 
     #region  blurStuff from tutorial
     const int maxPyramidSize = 16;
@@ -49,7 +51,7 @@ public class CustomPostProcessPass : ScriptableRenderPass
         }
         else
         {
-            hdrFormat = QualitySettings.activeColorSpace == ColorSpace.Linear 
+            hdrFormat = QualitySettings.activeColorSpace == ColorSpace.Linear
             ? GraphicsFormat.R8G8B8A8_SRGB
             : GraphicsFormat.R8G8B8A8_UNorm;
 
@@ -59,19 +61,40 @@ public class CustomPostProcessPass : ScriptableRenderPass
     public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
     {
         VolumeStack stack = VolumeManager.instance.stack;
-        m_render = stack.GetComponent<PixelPostProcessComponent>();
+        m_effect = stack.GetComponent<PixelPostProcessComponent>();
 
         CommandBuffer cmd = CommandBufferPool.Get();
 
-        using(new ProfilingScope(cmd, new ProfilingSampler("Custom Post Process Effects")))
+        using (new ProfilingScope(cmd, new ProfilingSampler("Custom Post Process Effects")))
         {
-            SetupBloom(cmd, cameraColorTarget); //stopped at https://youtu.be/9fa4uFm1eCE?si=bPLv4EnMKUwySzWd&t=818
+            SetupBloom(cmd, cameraColorTarget);
         }
+
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+
+        CommandBufferPool.Release(cmd);
     }
 
     public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
     {
-        descriptor = renderingData.cameraData.cameraTargetDescriptor;
+        m_Descriptor = renderingData.cameraData.cameraTargetDescriptor;
+    }
+
+    RenderTextureDescriptor GetCompatibleDescriptor()
+        => GetCompatibleDescriptor(m_Descriptor.width, m_Descriptor.height, m_Descriptor.graphicsFormat);
+
+    RenderTextureDescriptor GetCompatibleDescriptor(int width, int heigth, GraphicsFormat format, DepthBits depthBufferBits = DepthBits.None)
+        => GetCompatibleDescriptor(m_Descriptor, width, heigth, format, depthBufferBits);
+
+    public static RenderTextureDescriptor GetCompatibleDescriptor(RenderTextureDescriptor desc, int width, int height, GraphicsFormat format, DepthBits depthBufferBits = DepthBits.None)
+    {
+        desc.depthBufferBits = (int)depthBufferBits;
+        desc.msaaSamples = 1;
+        desc.width = width;
+        desc.height = height;
+        desc.graphicsFormat = format;
+        return desc;
     }
 
     public void SetTarget(RTHandle camerColorTargetHandle, RTHandle camerDepthTargetHandle)
@@ -79,4 +102,74 @@ public class CustomPostProcessPass : ScriptableRenderPass
         cameraColorTarget = camerColorTargetHandle;
         cameraDepthTarget = camerDepthTargetHandle;
     }
+
+
+    void SetupBloom(CommandBuffer cmd, RTHandle source)
+    {
+        // Start at half—res
+        int downres = 1;
+        int tw = m_Descriptor.width >> downres;
+        int th = m_Descriptor.height >> downres;
+
+        // Determine the iteration count
+        int maxSize = Mathf.Max(tw, th);
+        int iterations = Mathf.FloorToInt(Mathf.Log(maxSize, 26));
+        int mipCount = Mathf.Clamp(iterations, 1, m_effect.maxIterations.value);
+
+        // Pre—filtering parameters
+        float clamp = m_effect.clamp.value;
+        float threshold = Mathf.GammaToLinearSpace(m_effect.threshold.value);
+        float thresholdKnee = threshold * 0.5f; // Hardcoded soft knee
+
+        // Material setup
+        float scatter = Mathf.Lerp(0.05f, 0.95f, m_effect.scatter.value);
+        var bloomMaterial = m_render;
+
+        bloomMaterial.SetVector("_Params", new Vector4(scatter, clamp, threshold, thresholdKnee));
+
+        // Prefilter
+        var desc = GetCompatibleDescriptor(tw, th, hdrFormat);
+
+        for (int i = 0; i < mipCount; i++)
+        {
+            RenderingUtils.ReAllocateIfNeeded(ref m_BloomMipUp[i], desc, FilterMode.Point, TextureWrapMode.Clamp,
+                name: m_BloomMipUp[i].name);
+            RenderingUtils.ReAllocateIfNeeded(ref m_BloomMipDown[i], desc, FilterMode.Point, TextureWrapMode.Clamp,
+                name: m_BloomMipDown[i].name);
+            desc.width = Mathf.Max(1, desc.width >> 1);
+            desc.height = Mathf.Max(1, desc.height >> 1);
+        }
+
+        Blitter.BlitCameraTexture(cmd, source, m_BloomMipDown[0], RenderBufferLoadAction.DontCare,
+            RenderBufferStoreAction.Store, m_render, 0);
+
+
+        var lastDown = m_BloomMipDown[0];
+        for (int i = 0; i < mipCount; i++)
+        {
+            //Classic two pass gaussian blur — use mipUp as a temporary target
+            //First pass does 2x downsampling +9—tap gaussian
+            //Second pass does 9—tap gaussian using a 5—tap filter +bilinear filtering
+            Blitter.BlitCameraTexture(cmd, lastDown, m_BloomMipUp[i], RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store, bloomMaterial, 1);
+            Blitter.BlitCameraTexture(cmd, m_BloomMipUp[i], m_BloomMipDown[i], RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store, bloomMaterial, 2);
+
+            lastDown = m_BloomMipUp[i];
+        }
+
+        // Upsample (bilinear by default, HQ filtering does bicubic instead
+        for (int i = mipCount - 2; i >= 0; i--)
+        {
+            var lowMip = (i == mipCount - 2) ? m_BloomMipDown[i + 1] : m_BloomMipDown[i + 1];
+            var highMip = m_BloomMipDown[i];
+            var dst = m_BloomMipUp[i];
+
+            cmd.SetGlobalTexture("_SourceTexLowMip", lowMip);
+            Blitter.BlitCameraTexture(cmd, highMip, dst, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store, bloomMaterial, 3);
+
+        }
+
+        cmd.SetGlobalTexture("_BloomTexture", m_BloomMipUp[0]);
+        cmd.SetGlobalFloat("_BloomIntensity", m_effect.intensity.value);
+    }
 }
+
--- Assets/+++workdata/Scripts/PixelHelper/PixelPostProcess/PixelPostProcess.cs
+++ Assets/+++workdata/Scripts/PixelHelper/PixelPostProcess/PixelPostProcess.cs
@@ -9,7 +9,21 @@ public class PixelPostProcessComponent : VolumeComponent, IPostProcessComponent
     public ClampedFloatParameter PixelRes = new(550, 16, 1920, true);
     public NoInterpColorParameter outlineCol = new(Color.white);
     public FloatParameter outlineSize = new(1, true);
-    
+
+    #region BloomSettings
+    public FloatParameter threshold = new(.9f, true);
+    public FloatParameter intensity = new(1, true);
+    public ClampedFloatParameter scatter = new(.7f, 0, 1, true);
+    public IntParameter clamp = new IntParameter(65472, true);
+    public ClampedIntParameter maxIterations = new(6, 0, 10);
+    public NoInterpColorParameter tint = new(Color.white);
+
+    public IntParameter dotsDensity = new IntParameter(10, true);
+    public ClampedFloatParameter dotsCutoff = new(.4f, 0, 1, true);
+    Vector2Parameter scrollDirection = new(new());
+
+
+    #endregion
 
     public bool IsActive()
     {

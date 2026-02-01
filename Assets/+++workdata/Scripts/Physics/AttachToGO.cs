ae5ca31a6e81a2c0cd38b20408ecf4c1fd42001a
TomGrzembke <tomgrzembke04@gmail.com>
2026-02-01 14:46:08 +0100

    Setupped working cm cam blending

--- Assets/+++workdata/Scripts/Physics/AttachToGO.cs
+++ Assets/+++workdata/Scripts/Physics/AttachToGO.cs
@@ -1,11 +1,30 @@
+using MyBox;
 using UnityEngine;
 
 public class AttachToGO : MonoBehaviour
 {
-    [SerializeField] Transform attachTo;
+    [ConditionalField(nameof(attachToMainCam), true), SerializeField]
+    Transform attachTo;
+
+    [SerializeField] bool attachToMainCam;
+
+    Transform transToAttachTo;
+
+    private void Start()
+    {
+        RefreshAttachment();
+    }
+
+    private void RefreshAttachment()
+    {
+        if (transToAttachTo != null) return;
+
+        if (!attachToMainCam) transToAttachTo = attachTo;
+        else transToAttachTo = Camera.main.transform;
+    }
 
     void Update()
     {
-        transform.position = attachTo.position;
+        transform.position = transToAttachTo.position.RemoveZ();
     }
 }
